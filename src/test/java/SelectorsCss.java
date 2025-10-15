bf3a56d9db0cc9c169f4d81119845dac4f05466c
ly4sh <anatolimihaelov53@gmail.com>
2025-10-15 15:19:12 +0300

    created First.java

--- src/test/java/SelectorsCss.java
+++ src/test/java/SelectorsCss.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class SelectorsCss {
+
     WebDriver driver = new ChromeDriver();
 
     @Test
@@ -17,26 +18,85 @@ public class SelectorsCss {
         driver.get("https://telranedu.web.app/home");
         WebElement btnAbout = driver.findElement(By.cssSelector("a[href='/about']"));
         btnAbout.click();
+        pause(2);
         System.out.println(btnAbout.getText());
         WebElement btnLogin = driver.findElement(
                 By.cssSelector("div[id='root'] a:last-child"));
         btnLogin.click();
-        //=================================
-        WebElement inputEmail = driver.findElement(By.cssSelector("input[name='email']"));
-        inputEmail.sendKeys("tolyamail@gmail.com");
-        WebElement inputPassword = driver.findElement(By.cssSelector("*[placeholder='Password']"));
-        inputPassword.sendKeys("Qwerty123!");
-        WebElement btnLoginForm = driver.findElement(By.cssSelector("form>button[name='login']"));
+        //========================================
+        WebElement inputEmail = driver.findElement(
+                By.cssSelector("input[name='email']"));
+        inputEmail.sendKeys("anatolii@gmail.com");
+        WebElement inputPassword = driver.findElement(
+                By.cssSelector("*[placeholder='Password']"));
+        inputPassword.sendKeys("At123456$$");
+        WebElement btnLoginForm = driver.findElement(
+                By.cssSelector("form>button[name='login']"));
         btnLoginForm.click();
+        pause(5);
+
+        driver.quit();
+
 
     }
 
-    static void pause(int time) {
+    @Test
+    public void iLcarroTest() {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://ilcarro.web.app/search");
+
+        WebElement btnSearch = driver.findElement(
+                By.cssSelector("a[href='/search']"));
+        btnSearch.click();
+        pause(2);
+        System.out.println(btnSearch.getText());
+
+        WebElement btnLetCarWork = driver.findElement(
+                By.cssSelector("*[href='/let-car-work']"));
+        btnLetCarWork.click();
+        pause(2);
+        System.out.println(btnLetCarWork.getText());
+
+        WebElement btnTermOfUse = driver.findElement(
+                By.cssSelector("a[id='2']"));
+        btnTermOfUse.click();
+        pause(2);
+        System.out.println(btnTermOfUse.getText());
+
+        WebElement btnSignUpIlCarro = driver.findElement(
+                By.cssSelector("a[ng-reflect-router-link='registration']"));
+        btnSignUpIlCarro.click();
+        pause(2);
+        System.out.println(btnSignUpIlCarro.getText());
+
+        WebElement btnLoginIlCarro = driver.findElement(
+                By.cssSelector("div[class='header']  a:last-child"));
+        btnLoginIlCarro.click();
+        pause(2);
+        System.out.println(btnLoginIlCarro.getText());
+
+        WebElement inputEmailIlCarro = driver.findElement(
+                By.cssSelector("input[type='email']"));
+        inputEmailIlCarro.sendKeys("anatolii@gmail.com");
+        WebElement inputPasswordIlCarro = driver.findElement(
+                By.cssSelector("input[formcontrolname='password']"));
+        inputPasswordIlCarro.sendKeys("At123456$");
+        WebElement btnLoginFormIlCarro = driver.findElement(
+                By.cssSelector("button[type='submit']"));
+        btnLoginFormIlCarro.click();
+        pause(5);
+
+        driver.quit();
+
+    }
+
+    static void pause(int time){
         try {
-            Thread.sleep(time * 1000L);
+            Thread.sleep(time *1000L);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
-
 }
---
+++ src/test/java/SelectorsXPath.java
@@ -0,0 +1,115 @@
+
+    import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+
+    public class SelectorsXPath {
+        WebDriver driver = new ChromeDriver();
+
+        @Test
+        public void phoneBookTest() {
+            driver.manage().window().maximize();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.get("https://telranedu.web.app/home");
+            WebElement btnAbout = driver.findElement(
+                    By.xpath("//a[@href='/about']"));
+            btnAbout.click();
+            //btnAbout.sendKeys();
+            SelectorsCss.pause(3);
+            System.out.println(btnAbout.getText());
+            System.out.println(btnAbout.getTagName());
+            System.out.println(btnAbout.isEnabled());
+            System.out.println(btnAbout.isDisplayed());
+            WebElement btnLoginHeader = driver.findElement(
+                    By.xpath("//*[text() = 'LOGIN']"));
+            btnLoginHeader.click();
+            WebElement inputEmail = driver.findElement(
+                    By.xpath("//form/input[contains(@name, 'em')]"));
+            int i = new Random().nextInt(1000);    //0 - 999
+            String email = "new_bobs_email" + i + "@mail.com";  //new_bobs_email123@mail.com
+            inputEmail.sendKeys(email);
+            WebElement inputPassword = driver.findElement(
+                    By.xpath("//form/input[last()]"));
+            inputPassword.sendKeys("Password123!");
+
+            List<WebElement> buttons = driver.findElements(
+                    By.xpath("//button"));
+            System.out.println(buttons);
+            System.out.println(buttons.get(0).getText());
+            System.out.println(buttons.get(1).getText());
+            SelectorsCss.pause(3);
+            buttons.get(1).click();
+            WebElement btnSignOut = driver.findElement(
+                    By.xpath("//*[contains(text(),Out)]"));
+            SelectorsCss.pause(3);
+            btnSignOut.click();
+            driver.close();  //close window
+            //driver.quit(); //close  browser
+
+
+        }
+
+        @Test
+        public void iLcarroTest() {
+            driver.manage().window().maximize();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.get("https://ilcarro.web.app/search");
+
+            WebElement btnSearch = driver.findElement(
+                    By.xpath("//a[@href='/search']"));
+            SelectorsCss.pause(3);
+            btnSearch.click();
+
+            WebElement btnSignUp = driver.findElement(
+                    By.xpath("//a[text()='Sign up']"));
+            SelectorsCss.pause(3);
+            btnSignUp.click();
+            WebElement inputFirstName = driver.findElement(
+                    By.xpath("//input[@formcontrolname='firstName']"));
+            inputFirstName.sendKeys("Lordik");
+            WebElement inputLastName = driver.findElement(
+                    By.xpath("//input[@formcontrolname='lastName']"));
+            inputLastName.sendKeys("TTTTtlmkh");
+            WebElement inputEmailIlCarro = driver.findElement(
+                    By.xpath("//input[@formcontrolname='email']"));
+            inputEmailIlCarro.sendKeys("anatolii@gmail.com");
+            WebElement inputPasswordIlCarro = driver.findElement(
+                    By.xpath("//input[@formcontrolname='password']"));
+            inputPasswordIlCarro.sendKeys("At123456$");
+            WebElement inputCheckBox = driver.findElement(
+                    By.xpath("//input[@formcontrolname='termsOfUse']"));
+            WebElement btnYalla = driver.findElement(
+                    By.xpath("//*[text()='Y’alla!']"));
+            btnYalla.click();
+
+
+        /*int f = new Random().nextInt(1000);    //0 - 999
+        String emailIlCarro = "new_ilCarro_email" + f + "@mail.com";  //new_bobs_email123@mail.com
+        inputFirstName.sendKeys(emailIlCarro);
+        WebElement inputPassword = driver.findElement(
+                By.xpath("//form/input[last()]"));
+        inputPassword.sendKeys("Password123!");*/
+
+
+            WebElement btnLogIn = driver.findElement(
+                    By.xpath("//a[text()='Log in']"));
+            SelectorsCss.pause(3);
+            btnLogIn.click();
+
+
+
+
+
+
+        }
+
+    }
+
